6b5d407fcc106352b25d1e798537cfe2773a7600
Tyler Ewing <tewing10@gmail.com>
2014-06-15 20:13:40 -0400

    Added in some aliases

--- alias.zsh
+++ alias.zsh
@@ -1,2 +1,26 @@
 # Need to add alllllll my aliases here...
 # Yeah later....
+
+# Ping
+alias pingg='ping -c 3 google.com'
+alias pingd='ping -c 3 8.8.8.8'
+
+# List directory
+alias ll='ls -lahG'
+alias lls='ls -aG'
+alias lh='ls -d .*'
+
+# Changing directory
+alias ..='cd ..'
+alias ...='cd ../..'
+
+# Git
+alias ga='git add'
+alias gp='git push'
+alias gs='git status'
+alias gd='git diff'
+alias gm='git commit'
+alias gma='git commit -a'
+alias gc='git checkout'
+alias gl'git pull'
+
