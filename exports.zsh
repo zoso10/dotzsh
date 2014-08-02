4492513f1da4fd2b3b0219748328aee09b55e4b2
Tyler Ewing <tewing10@gmail.com>
2014-08-02 14:09:08 -0400

    Adds PGHOST export

--- exports.zsh
+++ exports.zsh
@@ -1,3 +1,5 @@
+export PATH=/usr/local/bin:$PATH # I may end up regretting this...
 export PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
 export PS1="[%*][%n@%M]$ "
 export EDITOR=vim
+export PGHOST=localhost
