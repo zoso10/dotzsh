b172f12c9710e690574df44b14a3ff95c2302722
Tyler Ewing <tewing10@gmail.com>
2014-10-23 20:22:09 -0400

    Add MySQL exports

--- exports.zsh
+++ exports.zsh
@@ -1,3 +1,8 @@
+# MySQL
+export MYSQL_HOME=/usr/local/mysql
+export PATH=$MYSQL_HOME/bin:$PATH
+
+# Prefer homebrew
 export PATH=/usr/local/bin:$PATH # I may end up regretting this...
 export PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
 export PS1="[%*][%n@%M]$ "
