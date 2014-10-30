dd3c47c8f4a1ee7ecb0048fe72730e29a2dd8529
Tyler Ewing <tewing10@gmail.com>
2014-10-30 18:13:22 -0400

    Add BUNDLER_EDITOR env variable

--- exports.zsh
+++ exports.zsh
@@ -7,4 +7,5 @@ export PATH=/usr/local/bin:$PATH # I may end up regretting this...
 export PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
 export PS1="[%*][%n@%M]$ "
 export EDITOR=vim
+export BUNDLER_EDITOR=vim
 export PGHOST=localhost
