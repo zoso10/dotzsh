8b001c778c3f3b4176f87e1e9b29df35ac798ce9
Tyler Ewing <tewing10@gmail.com>
2014-12-09 12:35:08 -0500

    Update Prompt

--- exports.zsh
+++ exports.zsh
@@ -5,7 +5,6 @@ export PATH=$MYSQL_HOME/bin:$PATH
 # Prefer homebrew
 export PATH=/usr/local/bin:$PATH # I may end up regretting this...
 export PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
-export PS1="[%*][%n@%M]$ "
 export EDITOR=vim
 export BUNDLER_EDITOR=vim
 export PGHOST=localhost
@@ -13,3 +12,16 @@ export PGHOST=localhost
 # Android
 export ANDROID_HOME=$HOME/Documents/adt-bundle-mac-x86_64-20140702/sdk
 export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
+
+# Prompt
+git_branch() {
+  ref=$(git symbolic-ref HEAD --short 2> /dev/null)
+
+  if [[ -n $ref ]]; then
+    echo  $ref
+  else
+    echo %1~
+  fi
+}
+
+export PROMPT='[%m] $fg_bold[red]:: $reset_color$(git_branch) $fg_bold[red]❯❯❯$reset_color '
