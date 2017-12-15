0f85e1a6d21a2e36a433451403d8c5f57277e2c6
Tyler Ewing <tewing10@gmail.com>
2017-12-15 16:12:45 -0500

    Clean up more exports

--- exports.zsh
+++ exports.zsh
@@ -1,24 +1,19 @@
-# MySQL
-export MYSQL_HOME=/usr/local/mysql
-export PATH=$MYSQL_HOME/bin:$PATH
-
 # Prefer homebrew
 export PATH=/usr/local/bin:$PATH # I may end up regretting this...
 export EDITOR=vim
 export BUNDLER_EDITOR=vim
 export PGHOST=localhost
 
-# Android
-export ANDROID_HOME=$HOME/Documents/adt-bundle-mac-x86_64-20140702/sdk
-export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
-
-# Postgres App
-export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/9.3/bin
+# Ruby
+export PATH=$PATH:$HOME/.rbenv/bin
 
 # Go
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
 
-# Ruby
-export PATH=$PATH:$HOME/.rvm/bin
-export PATH=$PATH:$HOME/.rbenv/bin
+# Postgres App
+export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/9.3/bin
+
+# MySQL
+export MYSQL_HOME=/usr/local/mysql
+export PATH=$MYSQL_HOME/bin:$PATH
