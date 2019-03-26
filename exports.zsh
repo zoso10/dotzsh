1de3b4840057791dc6bb178feeb33770b4af3fea
Tyler Ewing <tewing10@gmail.com>
2019-03-26 14:28:08 -0400

    Node version manager exports

--- exports.zsh
+++ exports.zsh
@@ -23,3 +23,8 @@ export PATH=$PATH:$HOME/.cargo/bin
 
 # GPG
 export GPG_TTY=$(tty)
+
+# NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
