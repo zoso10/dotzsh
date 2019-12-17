fe99fdf8c533b3997d6691bd8ce5ddbd39e29507
Tyler Ewing <tewing10@gmail.com>
2019-12-16 20:37:15 -0600

    Clean up a few exports

--- exports.zsh
+++ exports.zsh
@@ -1,5 +1,6 @@
 # Prefer homebrew
-export PATH=/usr/local/bin:$PATH # I may end up regretting this...
+export PATH=/usr/local/bin:$PATH
+
 export EDITOR=vim
 export BUNDLER_EDITOR=vim
 export PGHOST=localhost
@@ -10,9 +11,6 @@ export PATH=$PATH:$HOME/.rbenv/bin
 # Go
 export GOPATH=$HOME/.go
 export PATH=$PATH:$GOPATH/bin
-
-# Postgres App
-export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/9.3/bin
 
 # MySQL
 export MYSQL_HOME=/usr/local/mysql
