10efd2f66b500e439d03920cca05657e94507654
Tyler Ewing <tewing10@gmail.com>
2019-12-20 09:32:39 -0600

    Add tfenv to path

--- exports.zsh
+++ exports.zsh
@@ -27,4 +27,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+# Terraform
+export PATH=$HOME/.tfenv/bin:$PATH
+
 export HISTFILE=~/.histfile
